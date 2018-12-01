d633114115f41b557d9c58a151124d4e57bf6965
Mahdi Saffari <mmsaffari@gmail.com>
2018-12-01 17:32:56 +0330

    Day 01 is solved.

--- AOC 2018/Day 01.cs
+++ AOC 2018/Day 01.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AOC_2018 {
-	class Day_01 {
+	static class Day_01 {
+		private static int[] Input { get; set; } = new int[] { };
 
+		static Day_01() {
+			string input_file_path = Path.Combine(Program.Input_Files_Path, "day_01_part_1__input.txt");
+			Input = File.ReadAllLines(input_file_path)
+				.Where(l => !string.IsNullOrWhiteSpace(l))
+				.Select(l => Convert.ToInt32(l))
+				.ToArray();
+		}
 
+		public static int Calculate_Frequency_Drift() {
+			int result = 0;
+			result = Input.Sum();
+			return result;
+		}
+
+		public static int Find_Period_Frequecy() {
+			List<int> lFrequencies = new List<int>();
+			int index = 0;
+			int nCurrentFrequency = 0;
+			while (true) {
+				index %= Input.Length;
+				nCurrentFrequency += Input[index++];
+				if (lFrequencies.Any(f => f == nCurrentFrequency)) {
+					break;
+				}
+				lFrequencies.Add(nCurrentFrequency);
+			}
+			return nCurrentFrequency;
+		}
 	}
 }
--- AOC 2018/Program.cs
+++ AOC 2018/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Reflection;
 
 namespace AOC_2018 {
 	class Program {
+
+		internal static string Input_Files_Path { get => Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), @"..\..\Inputs")); }
+
 		static void Main(string[] args) {
+			System.Console.WriteLine("Day 1 - Part 1 - Frequency Drift: {0:N0}", Day_01.Calculate_Frequency_Drift());
+			System.Console.WriteLine("Day 1 - Part 2 - Period Frequency : {0:N0}", Day_01.Find_Period_Frequecy());
 		}
 	}
 }
